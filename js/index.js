da36e7e890fcd71daf5927e4d031ef7c37724d6f
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-07-08 12:34:38 -0400

    4topush

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-// Funcion saludar
+// ./js/index.js
 
-const saludar = ( )=>{
-    return "hola"
+// VARIABLES
+const mike = "Mike";
+
+const saludar = () => {
+  return "Hola!";
+};
+
+const resultado = saludar();
+
+console.log(resultado);
+
+if (1 === 1) {
+  console.log("Son iguales");
 }
 
-const resultado = saludar()
+switch ("México") {
+  case "México":
+    console.log("Es México");
+    break;
+  default:
+    console.log("No es México");
+}
 
-console.log(resultado)
+const sumar = (numero1, numero2) => numero1 + numero2;
 
+sumar(1, 5);
+
+const word = "corta";
+// CONCATENACIÓN
+const str1 = "Esta es" + " " + "una oración" + " " + word + ".";
+console.log(str1);
+
+// INTERPOLACIÓN
+const str2 = `Esta es una oración ${word}.`;
+console.log(str2);
